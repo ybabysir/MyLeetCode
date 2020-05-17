56e5c78abea63b2aeccf362da10f05835688730b
ybabysir <1065373753@qq.com>
2020-05-17 17:50:01 +0800

    first commit

---
+++ src/leetcode/Main.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+import leetcode.easy.SingleNumber1;
+
+public class Main {
+
+    public static void main(String[] args){
+//        easy-singlenumber
+//        System.out.println(SingleNumber1.singleNumber(new int[]{4,1,2,1,2}));
+    }
+}
